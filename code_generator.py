1997b59d641395ffd7ea16ca39ef5cf084f1735f
ObiTracks <straightup.stack@gmail.com>
2022-12-03 18:16:50 -0500

    Code generation scripting functions and mapping dict done

--- code_generator.py
+++ code_generator.py
@@ -1,26 +1,26 @@
 sign_mappings = {
-    0 : """
+    0: """
 for i in range(5):
     print("Hello World")
 """,
-    1 : """
+    1: """
 i = 0
 while i < 5:
     print("The Sun is Shining")
     i += 1""",
-    2 : """
+    2: """
 if (i == 0):
     print("The World is at Balance")
 """,
-    3 : """
+    3: """
 elif (i == 1):  
     print("The World is out of Balance")
 """,
-    4 : """
+    4: """
 else:
     print("The World is Back in Balance")
 """,
-    5 : """
+    5: """
 def some_function():
     x = ("Hello World")
     return x
@@ -35,27 +35,33 @@ def get_sign_mapping(sign: str):
     return x
 
 
-def write_code_to_file(code: str):
+def write_code_to_file(code: str, file_name: str = "output.py"):
     try:
-        with open('output.py', 'w+') as f:
+        with open(file_name, 'a') as f:
             f.write(code)
             print('Code written to output.py')
+        f.close()
     except Exception as e:
         print("Failed to write code to file: Error {}".format(e))
 
     return
 
 
-def main(sign_codes):
+def main(sign, output_filename):
     # Get whichever ASL sign has being displayed on the screen - will be a number from 0-7
-    code_output = ""
-    for sign_code in sign_codes:
-        sign = get_sign_mapping(sign_code)
-        code_output += sign
-
-    write_code_to_file(code_output)
+    sign = get_sign_mapping(sign)
+    write_code_to_file(sign, output_filename)
 
 
-# Testing the code
+
+#Output file to write code to
+output_filename = 'output.py'
+
+# Erasing the contents of the output file before running the code
+open(output_filename, 'w').close()
+
+# Testing the code with dummy array of sign codes
 sign_codes = [0, 1, 2, 3, 4, 5, ]
-main(sign_codes)
+code_output = ""
+for sign_code in sign_codes:
+    main(sign_code, output_filename)
